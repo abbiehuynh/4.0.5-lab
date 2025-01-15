74d7d745b48d956ab43ea06245a290e3ff79096b
abbiehuynh <abbiehuynhh@gmail.com>
2025-01-15 13:20:25 -0600

    Adds expected output in comments

--- src/main/java/academy/javapro/lab/LogicalOperators.java
+++ src/main/java/academy/javapro/lab/LogicalOperators.java
@@ -24,3 +24,20 @@ public class LogicalOperators {
 
     }
 }
+
+// Expected Output:
+/*  Logical AND:
+    Is it raining and weekend? false
+    Can go to the beach (sunny and weekend and has money)? true
+
+    Logical OR:
+    Stay home (raining or cold)? true
+    Go out (not raining or has umbrella)? true
+
+    Logical NOT:
+    Is it not raining? false
+    Is it not a weekday? true
+
+    Combined Logical Operators:
+    Complex condition ((sunny or has umbrella) and has money)? true
+*/
